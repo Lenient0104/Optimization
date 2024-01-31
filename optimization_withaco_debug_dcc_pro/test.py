4c234386b0af26c81fbf77df1819b8e15cac4d10
lenient <1617847148@qq.con>
2024-01-31 18:16:57 +0000

    pre computation

---
+++ optimization_withaco_debug_dcc_pro/test.py
@@ -0,0 +1,32 @@
+import unittest
+from user_info import User
+from optimization import Optimization
+
+
+class TestOptimization(unittest.TestCase):
+
+    def setUp(self):
+        self.net_xml_path = 'DCC.net.xml'
+        self.source_edge = '3789374#3'
+        self.target_edge = '41592114'
+        self.start_mode = 'walking'
+        self.ant_num = 2000
+        self.iteration = 1
+        self.user = User(60, False, 0)
+
+    def test_optimization_class(self):
+        # Initializing the Optimization class with the paths
+        db_path = 'test_new.db'
+        optimizer = Optimization(self.net_xml_path, self.user, db_path)
+        print("Initialize finished")
+        # best_path = optimizer.run_aco_algorithm(self.source_edge, self.target_edge, self.ant_num, self.iteration,
+        #                                         self.start_mode)
+        possible_paths = optimizer.find_all_routes_between(self.source_edge, self.target_edge)
+        print(possible_paths)
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
