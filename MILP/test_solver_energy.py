01e962a7536faf890859667b9d884814946f8ea9
Lenient0104 <1617847148@qq.com>
2024-09-08 13:32:43 +0100

    it finally works!

--- MILP/test_solver_energy.py
+++ MILP/test_solver_energy.py
@@ -75,50 +75,46 @@ class PreferenceGenerator:
     def __init__(self, G, station_types):
         self.G = G  # original graph
         self.station_types = station_types  # station_types = ['eb', 'es', 'ec', 'walk']
+        self.node_station_pair = {}
 
     def generate_node_preferences(self):
         preferred_station = {}
         # preferred_nodes = ['361409608#3', '3791905#2', '-11685016#2', '369154722#2', '244844370#0', '37721356#0',
-        # '74233405#1', '129774671#0', '23395388#5', '-64270141'] #random.sample(list(self.G.nodes),
-
-        # 20 preferred nodes
+        #                   '74233405#1', '129774671#0', '23395388#5', '-64270141'] #random.sample(list(self.G.nodes), self.num_preferred_nodes)
+        # 20
         preferred_nodes = ['361409608#3', '3791905#2', '-11685016#2', '369154722#2', '244844370#0', '37721356#0',
                            '74233405#1', '129774671#0', '23395388#5', '-64270141', '18927706#0', '-42471880',
                            '67138626#1', '41502636#0', '-75450412', '-23347664#1', '14151839#3', '-12341242#1',
                            '-13904652',
                            '-47638297#3']
-        # 50 preferred_nodes = ['-52881219#6', '3788262#0', '-36819763#3', '151482073', '-44432110#1', '-45825602#2',
-        # '-37721078#5', '-4834964#1', '59433202', '-441393227#1', '20485040#0', '29333277', '-38062537',
-        # '37812705#0', '3708641#0', '4934504#3', '-8232242#0', '254372188#2', '-114208220#0', '-370381577#2',
-        # '228694841', '-22716429#2', '129068838', '-191042862#1', '42636056', '7849019#2', '556290239#1',
-        # '-63844803#0', '303325200#2', '4395609#0', '-575044518', '77499019#1', '-38375201#6', '129211829#0',
-        # '-228700735', '23086918#0', '-4825259#1', '-481224216#4', '-5825856#7', '-7989926#3', '4385842#0',
-        # '30999830#4', '529166764#0', '334138114#8', '-326772508#1', '3708865#8', '25787652', '55668420#1',
-        # '55814730#3', '-561862256']
-
-        # 100 preferred_nodes = ['128521563#0', '401904747', '31897169#3', '3284827#0',
-        # '-52241896', '372838330#2', '-376465145#1', '432565295#5', '24644260#0', '4540245#3', '4934643#0',
-        # '25536089#1', '-67139161#4', '30988145#0', '-282195032#5', '-48205720#2', '-128450676#3', '-115656107',
-        # '75741007', '32346509', '3818299#3', '-128946815#7', '-111746918', '38864100#0', '31350775#4',
-        # '662531575#0', '-43870791#1', '44385285', '-44436781#1', '129211827#0', '94838278#4', '-43175828#0',
-        # '37747500#5', '-129211821#4', '-25761629', '125813654#1', '369359214', '369154720#2', '30583814#1',
-        # '-365945819#0', '4541293#0', '520012239#0', '81982578#10', '-7919284#10', '7919276#0', '-326874928#1',
-        # '147077493#0', '45619558', '-127220738', '-14047195', '-45530928#0', '-372245390#1', '23501248#4',
-        # '14047190#0', '-26150087#3', '-7977648#1', '55668414#1', '256024070#3', '-129211821#7', '3191572#5',
-        # '-369977727#1', '677070805#4', '-3191572#1', '553868718#1', '-48226178#0', '-369622780', '-20848460#1',
-        # '174092689#0', '228694841', '64270037#0', '4385874#1', '-238740928#2', '45530928#1', '-80478293#2',
-        # '432565295#3', '-317027066#0', '-3818568#3', '-39247679#3', '285487940', '-4540248#0', '-3708538#2',
-        # '-129211838', '231340188#0', '-13998367', '-3789251#3', '6653000#5', '317219094#1', '48454248#0',
-        # '13878225#0', '-495995815', '-94838278#4', '-38090686#2', '32201932', '-30454721#1', '-147463637#1',
-        # '-49728145#0', '143866317#0', '-14039614', '-636824321', '-228665722']
+        # 50
+        # preferred_nodes = ['-52881219#6', '3788262#0', '-36819763#3', '151482073', '-44432110#1', '-45825602#2',
+        #                    '-37721078#5', '-4834964#1', '59433202', '-441393227#1',
+        #                    '20485040#0', '29333277', '-38062537', '37812705#0', '3708641#0', '4934504#3', '-8232242#0',
+        #                    '254372188#2',
+        #                    '-114208220#0', '-370381577#2', '228694841', '-22716429#2', '129068838', '-191042862#1',
+        #                    '42636056', '7849019#2', '556290239#1', '-63844803#0', '303325200#2', '4395609#0',
+        #                    '-575044518', '77499019#1', '-38375201#6', '129211829#0', '-228700735', '23086918#0',
+        #                    '-4825259#1', '-481it224216#4', '-5825856#7', '-7989926#3', '4385842#0', '30999830#4',
+        #                    '529166764#0', '334138114#8', '-326772508#1', '3708865#8', '25787652', '55668420#1',
+        #                    '55814730#3', '-561862256']
+        # 100
+        # preferred_nodes = ['128521563#0', '401904747', '31897169#3', '3284827#0', '-52241896', '372838330#2', '-376465145#1', '432565295#5', '24644260#0', '4540245#3', '4934643#0', '25536089#1', '-67139161#4', '30988145#0', '-282195032#5', '-48205720#2', '-128450676#3', '-115656107', '75741007', '32346509', '3818299#3', '-128946815#7', '-111746918', '38864100#0', '31350775#4', '662531575#0', '-43870791#1', '44385285', '-44436781#1',
+        #                    '129211827#0', '94838278#4', '-43175828#0', '37747500#5', '-129211821#4', '-25761629', '125813654#1', '369359214', '369154720#2', '30583814#1', '-365945819#0', '4541293#0', '520012239#0', '81982578#10', '-7919284#10', '7919276#0', '-326874928#1', '147077493#0', '45619558', '-127220738', '-14047195', '-45530928#0', '-372245390#1', '23501248#4', '14047190#0', '-26150087#3', '-7977648#1', '55668414#1', '256024070#3', '-129211821#7',
+        #                    '3191572#5', '-369977727#1', '677070805#4', '-3191572#1', '553868718#1', '-48226178#0', '-369622780', '-20848460#1', '174092689#0', '228694841', '64270037#0', '4385874#1', '-238740928#2', '45530928#1', '-80478293#2', '432565295#3', '-317027066#0', '-3818568#3', '-39247679#3', '285487940', '-4540248#0', '-3708538#2', '-129211838', '231340188#0', '-13998367', '-3789251#3', '6653000#5', '317219094#1', '48454248#0', '13878225#0', '-495995815',
+        #                      '-94838278#4', '-38090686#2', '32201932', '-30454721#1', '-147463637#1', '-49728145#0', '143866317#0', '-14039614', '-636824321', '-228665722']
 
         for i in self.G.nodes:
             if i in preferred_nodes:
                 # num_preferred = random.randint(1, len(self.station_types) - 1)
-                preferred_types = [st for st in self.station_types if st != 'walk']
+                # preferred_types = [st for st in self.station_types if st != 'walk']
+                # 为每个站点生成唯一的 station_types 列表
+                num_preferred = random.randint(1, len(self.station_types))  # 随机生成 1 到全部 station_types
+                preferred_types = random.sample(self.station_types, num_preferred)
                 preferred_station[i] = preferred_types  # random.sample(preferred_types, num_preferred)
                 if 'walk' not in preferred_station[i]:
                     preferred_station[i].append('walk')
+                self.node_station_pair[i] = preferred_station[i]
             else:
                 preferred_station[i] = ['']
 
@@ -143,6 +139,7 @@ class OptimizationProblem:
         self.station_change_costs = {}
 
     def setup_model(self):
+        # 初始化 Gurobi 模型
         self.model = gp.Model("Minimize_Traversal_Cost")
 
     def setup_decision_variables(self):
@@ -162,10 +159,10 @@ class OptimizationProblem:
                         self.station_changes[i, s1, s2] = self.model.addVar(vtype=GRB.BINARY, name=var_name)
 
         initial_energy = {  # in wh
-            'eb': 100000000000,
-            'es': 1000,
-            'ec': 1500,
-            'walk': 0
+            'eb': 50,
+            'es': 35,
+            'ec': 3000,
+            'walk': 100
         }
         for i in self.G.nodes:
             self.energy_vars[i] = {}
@@ -189,8 +186,9 @@ class OptimizationProblem:
                         self.costs[i, j, s] = edge_weight / speed if speed != 0 else 1e7
                 else:
                     pedestrian_speed = speeds.get('pedestrian_speed', 0)
-                    self.costs[i, j, s] = edge_weight  / pedestrian_speed if pedestrian_speed != 0 else 1e7
+                    self.costs[i, j, s] = edge_weight / pedestrian_speed if pedestrian_speed != 0 else 1e7
 
+        # 设置站点转换的成本
         for i in self.G.nodes:
             if len(self.preferred_station[i]) > 1:
                 for s1 in self.preferred_station[i]:
@@ -216,16 +214,13 @@ class OptimizationProblem:
                 self.energy_constraints[i, j, s] = self.calculate_energy(s, edge_weight)
 
     def setup_problem(self, start_node, start_station, end_node, end_station, max_station_changes):
-        # 设置目标函数
         obj = gp.quicksum(self.paths[i, j, s] * self.costs[i, j, s] for i, j, s in self.paths) + \
               gp.quicksum(self.station_changes[i, s1, s2] * self.station_change_costs[i, s1, s2] for i, s1, s2 in
                           self.station_changes)
         self.model.setObjective(obj, GRB.MINIMIZE)
 
-        # 添加流量平衡约束和特殊处理
         for i in self.G.nodes:
             for s in self.node_stations[i]:
-                # 计算流量
                 incoming_flow = gp.quicksum(
                     self.paths[j, i, s] for j in self.G.predecessors(i) if (j, i, s) in self.paths)
                 outgoing_flow = gp.quicksum(
@@ -239,7 +234,6 @@ class OptimizationProblem:
                 incoming_flow += incoming_station_changes
                 outgoing_flow += outgoing_station_changes
 
-                # 起点和终点的特殊处理
                 if i == start_node and s == start_station:
                     self.model.addConstr(outgoing_flow == 1, name=f"start_outflow_{i}_{s}")
                     self.model.addConstr(incoming_flow == 0, name=f"start_inflow_{i}_{s}")
@@ -249,58 +243,34 @@ class OptimizationProblem:
                 else:
                     self.model.addConstr(incoming_flow == outgoing_flow, name=f"flow_balance_{i}_{s}")
 
-        # 限制最大站点转换次数
         self.model.addConstr(gp.quicksum(self.station_changes.values()) <= max_station_changes,
                              name="max_station_changes")
 
-        # 添加路径和能量消耗的约束
         for i, j in self.G.edges():
             for s in set(self.node_stations[i]).intersection(self.node_stations[j]):
-                # print(f"Energy consumption for {i} -> {j} using {s}: {self.energy_constraints[i, j, s]}")
                 energy_consumption = self.energy_constraints[i, j, s]
 
-                # 创建辅助变量 can_traverse 表示路径是否可行
-                can_traverse = self.model.addVar(vtype=GRB.BINARY, name=f"can_traverse_{i}_{j}_{s}")
-
-                # 约束：如果能量足够，can_traverse == 1，否则 == 0
                 self.model.addConstr(
-                    self.energy_vars[i][s] - energy_consumption * can_traverse >= 0,
-                    name=f"EnergyFeasibility_{i}_{j}_{s}"
+                    self.paths[i, j, s] * energy_consumption <= self.energy_vars[i][s],
+                    name=f"PathEnergyFeasibility_{i}_{j}_{s}"
                 )
+                print(self.energy_vars[i][s])
 
-                # 约束：can_traverse 必须为 1 才能选择该路径
                 self.model.addConstr(
-                    self.paths[i, j, s] <= can_traverse,
-                    name=f"PathFeasibility_{i}_{j}_{s}"
-                )
-
-                # 更新能量：只有当路径可行时，才相减
-                self.model.addConstr(
-                    self.energy_vars[j][s] == self.energy_vars[i][s] - energy_consumption * self.paths[
-                        i, j, s] * can_traverse,
+                    self.energy_vars[j][s] >= self.energy_vars[i][s] - energy_consumption * self.paths[i, j, s],
                     name=f"EnergyConsumption_{i}_{j}_{s}"
                 )
+                self.model.update()
+                print(self.energy_vars[i][s])
 
-        # 能量重置逻辑
-        initial_energy = {  # in wh
-            'eb': 1000000000,
-            'es': 1000,
-            'ec': 3000,
-            'walk': 0
-        }
+        # energy reset
+        initial_energy = 50
         for i in self.G.nodes:
-            for s1 in self.node_stations[i]:
-                for s2 in self.node_stations[i]:
+            for s1 in self.preferred_station[i]:
+                for s2 in self.preferred_station[i]:
                     if s1 != s2:
-                        print(s1, s2)
-                        # 如果发生站点转换，重置新的交通工具的能量
                         self.model.addConstr(
-                            self.energy_vars[i][s2] >= self.station_changes[i, s1, s2] * initial_energy[s2],
-                            name=f"EnergyReset_{i}_{s1}_{s2}"
-                        )
-                        # 如果发生站点切换，重置新的交通工具的能量
-                        self.model.addConstr(
-                            self.energy_vars[i][s2] == self.station_changes[i, s1, s2] * initial_energy[s2],
+                            self.energy_vars[i][s2] >= self.station_changes[i, s1, s2] * initial_energy,
                             name=f"EnergyReset_{i}_{s1}_{s2}"
                         )
 
@@ -317,10 +287,6 @@ class OptimizationProblem:
             print("Model is unbounded.")
         else:
             print(f"Optimization ended with status {self.model.status}")
-
-        for (i, j, s), var in self.paths.items():
-            if var.X > 0.5:
-                print(f"Path selected: {i} -> {j} using {s}")
 
         return self.model, end_time - start_time
 
@@ -694,11 +660,17 @@ class RouteFinder:
         return complete_route_with_weights
 
 
-# Run code
+######################O*****************Original END *********************
+##########################################################################
+
+
+#################PipeLine to Execute the OD pairs from CSV ###################
+
+# Main code
 file_path = "/Users/dingyue/Documents/Optimization_new/MILP/DCC.net.xml"
 speed_file_path = '/Users/dingyue/Documents/Optimization_new/MILP/query_results-0.json'
 od_pairs_file = '/Users/dingyue/Documents/Optimization_new/MILP/od_pairs.csv'  # Path to the CSV file containing OD pairs
-output_csv_file = 'RG_TimeTest_50_Nodes_0906.csv'  # Output CSV file to store the results
+output_csv_file = 'RG_TimeTest_50_Nodes.csv'  # Output CSV file to store the results
 
 # Create graph from XML file
 graph_handler = GraphHandler(file_path)
@@ -706,11 +678,14 @@ original_G = graph_handler.get_graph()
 
 # Define parameters
 num_nodes = len(original_G.nodes)
+# User preferences
+user_preference = ['eb', 'ec', 'es']
 station_types = ['eb', 'es', 'ec', 'walk']
 node_stations = {i: station_types for i in original_G.nodes}
-# start_node = '-375581293#1'
-# end_node = '369977729#1'
-
+start_node = '-375581293#1'
+end_node = '369977729#1'
+node_stations[start_node] = ['walk']
+node_stations[end_node] = ['walk']
 no_pref_nodes = 10
 max_station_changes = 5
 M = 1e6
@@ -736,8 +711,7 @@ speed_dict = {entry['edge_id']: {'pedestrian_speed': float(entry['pedestrian_spe
                                  'car_speed': float(entry['car_speed'])}
               for entry in speed_data}
 
-# User preferences
-user_preference = ['eb', 'ec', 'es']
+
 
 # Prepare the output CSV file
 with open(output_csv_file, 'w', newline='') as csvfile:
@@ -754,9 +728,6 @@ with open(output_csv_file, 'w', newline='') as csvfile:
             initial_time = time.time()
 
             start_node, end_node = row  # Extract start_node and end_node from the current row
-
-            node_stations[start_node] = ['walk']
-            node_stations[end_node] = ['walk']
 
             # Compute shortest route pairs for testing only - otherwise outside the loop
             all_shortest_routes_pairs = shortest_path_computer.compute_shortest_paths_pairs(preferred_nodes)
@@ -787,7 +758,6 @@ with open(output_csv_file, 'w', newline='') as csvfile:
             try:
                 # Solve the problem and measure execution time
                 prob, execution_time = optimization_problem.solve()
-
                 optimization_problem.model.write("mymodel.lp")
 
                 #     if optimization_problem.model.status == GRB.INFEASIBLE:
@@ -811,6 +781,9 @@ with open(output_csv_file, 'w', newline='') as csvfile:
 
                     end_time = time.time()
                     Total_time = end_time - initial_time
+
+                    # route_with_weights = route_finder.get_complete_route_with_weights(path_sequence, shortest_routes_start, shortest_routes_dest, all_shortest_routes_pairs, shortest_route_start_end)
+                    # Write results to CSV
                     writer.writerow([start_node, end_node, total_cost, Total_time, path_sequence])
                 else:
                     # Write results to CSV with 'inf' for total cost if no optimal solution is found
